2d9127c1b5ba77a0ebed9d7b881490077c744661
Mathiascrepin <mgandjidon@gmail.com>
2023-06-14 12:24:53 +0200

    commit 1

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Automatically load required Grunt tasks
+    require('jit-grunt')(grunt);
+
+    // Define the configuration for all the tasks
